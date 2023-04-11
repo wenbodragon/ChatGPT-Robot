5bd48474817fa900c0b6e8a9381274c5e200fb59
LongWenbo <longwenbo@126.com>
2023-04-11 22:38:34 +0800

    chat

---
+++ chat-dog/src/components/Chat.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+function chat() {
+}
+</script>
+
+<template>
+<van-button plain type="primary" @click="chat">chat</van-button>
+</template>
--- chat-dog/src/main.ts
+++ chat-dog/src/main.ts
@@ -5,10 +5,12 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import { Button } from 'vant';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Button);
 
 app.mount('#app')
