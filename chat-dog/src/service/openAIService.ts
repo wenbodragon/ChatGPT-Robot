e6d9a85b8af10f1e09de9b80a05db6afa7618129
zaizaizhao <2534243725@qq.com>
2023-04-13 20:48:17 +0800

    配置env

--- chat-dog/src/service/openAIService.ts
+++ chat-dog/src/service/openAIService.ts
@@ -1,17 +1,21 @@
 import { Configuration, OpenAIApi } from 'openai'
-export async function callOpenAi(content: string): Promise<string> {
+console.log(import.meta.env);
+
+export async function callOpenAi(content: string) {
   const configuration = new Configuration({
-    apiKey: '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'
+    apiKey: import.meta.env.VITE_APP_KEY,
   })
+  console.log(import.meta.env.VITE_APP_KEY);
+
   const openai = new OpenAIApi(configuration)
 
-  const completion = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [{ role: 'user', content: content }]
-  })
-  if (completion.data.choices[0].message?.content) {
-    return completion.data.choices[0].message.content
-  } else {
-    return ''
-  }
+  // const completion = await openai.createChatCompletion({
+  //   model: 'gpt-3.5-turbo',
+  //   messages: [{ role: 'user', content: content }]
+  // })
+  // if (completion.data.choices[0].message?.content) {
+  //   return completion.data.choices[0].message.content
+  // } else {
+  //   return ''
+  // }
 }
