8ff0ec0c34f5c8fe8ad5b21586b3c884b8363f06
wenbo.long <wenbo.long@D1301004665>
2023-04-27 15:36:06 +0800

    elementui2vant

--- chat-dog/src/views/chatRoom/ChatRoom.vue
+++ chat-dog/src/views/chatRoom/ChatRoom.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { ref } from 'vue'
 import { callOpenAi } from '@/service/openAIService'
 
@@ -38,45 +38,188 @@ function readStream(reader:any){
   })
 }
 </script>
-
+ -->
 <template>
-  <div class="container">
-    <div class="avatar">
-      <el-avatar src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" />
+  <div class="chat-window">
+    <nav-bar title="聊天窗口" left-text="返回" left-arrow @click-left="onBackClick" />
+    <div class="chat-container" ref="chatContainer">
+      <div v-for="(msg, index) in messages" :key="index" class="message-wrapper">
+        <notice-bar v-if="msg.isNewDay" :text="formatDate(msg.time)" />
+        <div
+          class="message-content"
+          :class="{ me: msg.sender === 'me', others: msg.sender !== 'me' }"
+        >
+          <div v-if="msg.sender !== 'me'" class="avatar"></div>
+          <div class="content">{{ msg.content }}</div>
+          <div class="time">{{ formatTime(msg.time) }}</div>
+        </div>
+      </div>
     </div>
-    <div class="input">
-      <el-input v-loading="isLoading" v-model="response" :rows="2" type="textarea" />
-    </div>
-  </div>
-
-  <div class="container">
-    <div class="input">
-      <el-input v-model="question" :rows="2" type="textarea" placeholder="Ask dog a question" />
-    </div>
-    <div class="avatar">
-      <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
-    </div>
-  </div>
-
-  <div class="btn">
-    <el-button type="primary" @click="chatFun">chat</el-button>
+    <field
+      v-model="newMessage"
+      placeholder="请输入消息"
+      clearable
+      @blur="onInputBlur"
+      :autofocus="false"
+      :maxlength="maxMessageLength"
+    />
+    <button class="send-btn" @click="sendMessage">发送</button>
   </div>
 </template>
 
+<script setup lang="ts">
+import type { Ref } from 'vue'
+import { ref } from 'vue'
+import { callOpenAi } from '@/service/openAIService'
+// import { NavBar, NoticeBar, Field } from 'vant';
+
+interface Message {
+  content: string
+  sender: string
+  time: string
+  isNewDay: boolean
+}
+
+const response = ref('')
+
+const messages: Ref<Message[]> = ref([
+  {
+    content: '你好啊',
+    sender: 'other',
+    time: '2022-01-20 09:01:00',
+    isNewDay: false
+  },
+  {
+    content: '你好，很高兴见到你。',
+    sender: 'me',
+    time: '2022-01-20 09:02:00',
+    isNewDay: false
+  },
+  {
+    content: '最近怎么样？',
+    sender: 'other',
+    time: '2022-01-20 09:03:00',
+    isNewDay: false
+  }
+])
+
+const newMessage: Ref<string> = ref('')
+const maxMessageLength: number = 100
+
+function onBackClick(): void {
+  // 返回上一页代码...
+}
+
+function sendMessage(): void {
+  // 发送消息代码...
+  if (newMessage.value.trim() === '') {
+    return;
+  }
+  messages.value.push({
+    content: newMessage.value,
+    sender: 'me',
+    time: new Date().toLocaleString(),
+    isNewDay: false
+  })
+}
+
+function onInputBlur(): void {
+  // 输入框失焦代码...
+}
+
+function formatDate(dateStr: string): string {
+  // 格式化日期代码...
+  return 'Today'
+}
+
+function formatTime(timeStr: string): string {
+  // 格式化时间代码...
+  return timeStr.slice(-8)
+}
+
+async function chatFun() {
+  // isLoading.value = true
+  await callOpenAi(newMessage.value).then(res => {
+    //@ts-ignore
+    const reader = res.body.getReader();
+    // console.log(reader);
+    readStream(reader);
+  })
+}
+function readStream(reader:any){
+    //@ts-ignore
+    reader.read().then(({ done, value }) => {
+      if(done){
+        return
+      }
+      let decoded = new TextDecoder().decode(value);
+      let decodedArray = decoded.split("data: ");
+      decodedArray.forEach(decoded => {
+            if(decoded!==""){
+              if(decoded.trim()==="[DONE]"){
+                return;
+              }else{
+                const resString = JSON.parse(decoded).choices[0].delta.content ? 
+                JSON.parse(decoded).choices[0].delta.content : "";
+                response.value += resString
+              }
+            }
+          })
+      console.log(decoded);
+      return readStream(reader)
+  })
+}
+</script>
+
 <style scoped>
-.container {
+.chat-window {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background-color: #f7f9fa;
+}
+
+.chat-container {
+  flex-grow: 1;
+  overflow-y: scroll;
+  padding: 10px;
+}
+
+.message-wrapper {
+  margin-bottom: 10px;
+}
+
+.avatar {
+  width: 40px;
+  height: 40px;
+  background-color: #eee;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+
+.content {
+  padding: 10px;
+  background-color: #fff;
+  border-radius: 10px;
+}
+
+.time {
+  font-size: 12px;
+  color: #999;
+  margin-top: 5px;
+}
+
+.me .message-content {
+  display: flex;
+  flex-direction: row-reverse;
+}
+
+.others .message-content {
   display: flex;
   flex-direction: row;
 }
-.avatar {
-  flex: 1; /* 等分剩余空间 */
-}
 
-.input {
-  flex: 8; /* 等分剩余空间 */
-}
-.btn {
-  display: flex;
-  justify-content: flex-end;
+.send-btn {
+  padding: 10px;
 }
 </style>
--- chat-dog/vite.config.ts
+++ chat-dog/vite.config.ts
@@ -2,16 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'node:path'
 import Components from 'unplugin-vue-components/vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
     Components({
-      resolvers: [ElementPlusResolver()]
+      resolvers: [VantResolver()]
     })
   ],
   resolve: {
